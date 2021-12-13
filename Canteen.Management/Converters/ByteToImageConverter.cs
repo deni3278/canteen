7461c4e14da5ef92789a92d1ab8018dba60a0530
VNDPrncss <vanda.bovina@gmail.com>
2021-12-13 13:22:15 +0100

    Add missing files.

---
+++ Canteen.Management/Converters/ByteToImageConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace Canteen.Management.Converters;
+
+public class ByteToImageConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var bitmap = value as byte[];
+        var image = new BitmapImage();
+        using (var stream = new MemoryStream(bitmap!))
+        {
+            stream.Position = 0;
+            
+            image.BeginInit();
+            image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+            image.CacheOption = BitmapCacheOption.OnLoad;
+            image.StreamSource = stream;
+            image.EndInit();
+        }
+        image.Freeze();
+        return image;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Canteen.Management/Views/LunchTabView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+using Canteen.Management.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Canteen.Management.Views;
+
+public partial class LunchTabView : UserControl
+{
+    public LunchTabView()
+    {
+        InitializeComponent();
+
+        DataContext = App.Current.Services.GetService<LunchTabViewModel>();
+    }
+}
